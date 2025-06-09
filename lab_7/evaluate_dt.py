6558d9995ef8d5491ff16e4014dfca6948c0da2a
wozniakos10 <wozniakos10@gmail.com>
2025-06-09 21:18:46 +0200

    Plots lab7

--- lab_7/evaluate_dt.py
+++ lab_7/evaluate_dt.py
@@ -2,8 +2,9 @@ import argparse
 import numpy as np
 from nanodt.agent import NanoDTAgent
 import gymnasium as gym
+import json
 
-def evaluate(path_to_model: str, n_episodes: int):
+def evaluate(path_to_model: str, n_episodes: int, path_to_save: str):
     env = gym.make("LunarLander-v3", continuous=True)
     agent = NanoDTAgent.load(path_to_model)
     rewards = []
@@ -25,17 +26,37 @@ def evaluate(path_to_model: str, n_episodes: int):
 
         rewards.append(accumulated_rew)
 
-    print("\nEvaluation Results:")
-    print(f"Mean reward:   {np.mean(rewards):.2f}")
-    print(f"Std reward:    {np.std(rewards):.2f}")
-    print(f"Min reward:    {np.min(rewards):.2f}")
-    print(f"Max reward:    {np.max(rewards):.2f}")
-    print(f"Median reward: {np.median(rewards):.2f}")
+    mean = np.mean(rewards)
+    std = np.std(rewards)
+    max_reward = np.max(rewards)
+    min_reward = np.min(rewards)
+
+    print(f"---------stats---------")
+    print(f"Mean reward: {mean}")
+    print(f"Max reward: {max_reward}")
+    print(f"Min reward: {max_reward}")
+    print(f"Std reward: {std}")
+    print(f"---------stats---------")
+
+
+    stats = {
+        "mean_reward": mean,
+        "max_reward": max_reward,
+        "min_reward": min_reward,
+        "std_reward": std,
+        "rewards": rewards
+    }
+
+    with open(path_to_save, "w") as f:
+        json.dump(stats, f, indent=4)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Evaluate NanoDT agent on LunarLanderContinuous")
     parser.add_argument("--path_to_model", type=str, required=True, help="Path to the trained NanoDT model")
     parser.add_argument("--n_episodes", type=int, default=100, help="Number of evaluation episodes")
+    parser.add_argument("--path_to_save", type=str, required=True, help="Path to the saved model")
+
 
     args = parser.parse_args()
-    evaluate(args.path_to_model, args.n_episodes)
+    evaluate(args.path_to_model, args.n_episodes, args.path_to_save)
--- lab_7/evaluate_sd3.py
+++ lab_7/evaluate_sd3.py
@@ -2,8 +2,10 @@ import argparse
 import gymnasium as gym
 from stable_baselines3 import SAC
 from stable_baselines3.common.evaluation import evaluate_policy
+import numpy as np
+import json
 
-def main(path_to_model):
+def main(path_to_model: str, path_to_save: str):
     # Create environment
     env = gym.make("LunarLander-v3", continuous=True, render_mode="rgb_array")
 
@@ -11,8 +13,29 @@ def main(path_to_model):
     model = SAC.load(path_to_model, env=env)
 
     # Evaluate the agent
-    mean_reward, std_reward = evaluate_policy(model, model.get_env(), n_eval_episodes=100)
-    print(f"Mean reward: {mean_reward:.2f} +/- {std_reward:.2f}")
+    rewards, episodes_length = evaluate_policy(model, model.get_env(), n_eval_episodes=1000, return_episode_rewards=True)
+    mean = np.mean(rewards)
+    std = np.std(rewards)
+    max_reward = np.max(rewards)
+    min_reward = np.min(rewards)
+
+    print(f"---------stats---------")
+    print(f"Mean reward: {mean}")
+    print(f"Max reward: {max_reward}")
+    print(f"Min reward: {max_reward}")
+    print(f"Std reward: {std}")
+    print(f"---------stats---------")
+
+    stats = {
+        "mean_reward": mean,
+        "max_reward": max_reward,
+        "min_reward": min_reward,
+        "std_reward": std,
+        "rewards": rewards
+    }
+
+    with open(path_to_save, "w") as f:
+        json.dump(stats, f, indent=4)
 
     # Enjoy trained agent
     vec_env = model.get_env()
@@ -25,6 +48,7 @@ def main(path_to_model):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Evaluate a trained SAC agent on LunarLanderContinuous")
     parser.add_argument("--path_to_model", type=str, required=True, help="Path to the trained model")
+    parser.add_argument("--path_to_save", type=str, required=True, help="Path to the saved model")
 
     args = parser.parse_args()
-    main(args.path_to_model)
+    main(args.path_to_model, args.path_to_save)
